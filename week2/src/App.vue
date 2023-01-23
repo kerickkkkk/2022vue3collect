72c5a0385996ad5fc5da1bbd732b063a5a35cae0
eric <howcrazys@gmail.com>
2023-01-23 16:22:45 +0800

    feat: 修正路徑

--- week2/src/App.vue
+++ week2/src/App.vue
@@ -1,29 +1,35 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+import {onMounted} from 'vue'
+const baseUrl = import.meta.env.VITE_BASE_URL
+const path = import.meta.env.VITE_VITE_PATH
+
+onMounted( ()=> {
+  console.log(baseUrl)
+  axios.get(`${baseUrl}/api/${path}/products/all`)
+    .then(res=>{
+      console.log(res)
+    })
+  // const config = {
+  //   headers: { Authorization: token },
+  // };
+  // axios
+  //   .post('/V2/api/user/check', {}, config)
+  //     .then((res) => {
+  //       console.log(res.data);
+  //     }).catch((error) => {
+  //       console.log(error.response.data);
+  //     })
+          
+})
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
--- week2/vite.config.js
+++ week2/vite.config.js
@@ -3,5 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/2022vue3collect/week2/dist//',
   plugins: [vue()],
 })
