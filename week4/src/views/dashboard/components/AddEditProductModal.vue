c5595907ee974ccf1c9369f8f6cfecfc523c4143
eric <howcrazys@gmail.com>
2023-02-20 08:54:16 +0800

    temp

--- week4/src/views/dashboard/components/AddEditProductModal.vue
+++ week4/src/views/dashboard/components/AddEditProductModal.vue
@@ -1,110 +1,44 @@
 <script setup>
+import { ref, inject } from "vue";
 import axios from "axios";
-import { ref, onMounted, inject } from "vue";
-import { useRouter } from "vue-router";
 import Modal from "bootstrap/js/dist/modal";
-import Pagination from "../../components/base/PaginationComponent.vue";
 import { useStatusStore } from "@/stores/statusStore.js";
 
-const router = useRouter();
 const statusStore = useStatusStore();
 const swal = inject("$swal");
-
 const { setLoading } = statusStore;
 const baseUrl = import.meta.env.VITE_BASE_URL;
 const apiPath = import.meta.env.VITE_PATH;
 
-const productObj = {
-  category: "",
-  content: "",
-  description: "",
-  imageUrl: "",
-  imagesUrl: [],
-  is_enabled: "",
-  num: "",
-  origin_price: 0,
-  price: 0,
-  title: "",
-  unit: "",
-};
-
-const products = ref([]);
-const tempProduct = ref({ ...productObj });
-const pagination = ref(null);
-const modalType = ref("");
 const productModalDom = ref(null);
-const productModal = ref(null);
-const delProductModalDom = ref(null);
-const delProductModal = ref(null);
-// 暫存多圖的連結
-const imagesUrl = ref("");
-const getProducts = (page) => {
-  setLoading(true);
-  axios
-    .get(`${baseUrl}/api/${apiPath}/admin/products?page=${page}`)
-    .then(({ data }) => {
-      products.value = data.products;
-      pagination.value = data.pagination;
-    })
-    .catch((error) => {
-      setLoading(false);
-      console.dir(error);
-      swal(error?.response?.data);
-      // 在確認沒有權限的寫法
-      router.push({ name: "login" });
-    })
-    .finally(() => setLoading(false));
-};
-const getProduct = (id) => {
-  tempProduct.value = products.value.find((item) => item.id === id);
-};
-const openModalHandler = (type, id) => {
-  modalType.value = type;
-  if (type === "new" || type === "edit") {
-    if (type === "edit") {
-      getProduct(id);
-    }
-    productModal.value.show();
-  } else if (type === "delete") {
-    tempProduct.value.id = id;
-    delProductModal.value.show();
-  } else {
-    swal("錯誤");
-  }
-};
-// const addEditProduct = (id) => {
-//   const method = modalType.value === "new" ? "post" : "put";
-//   const url =
-//     modalType.value === "new"
-//       ? `${baseUrl}/api/${apiPath}/admin/product`
-//       : `${baseUrl}/api/${apiPath}/admin/product/${id}`;
-//   const data = { data: tempProduct.value };
-//   setLoading(true);
-//   axios({
-//     method,
-//     url,
-//     data,
-//   })
-//     .then(({ data }) => {
-//       if (data.success) {
-//         setLoading(false);
-//         swal(data.message);
-//         resetProduct();
-//         getProducts();
-//         productModal.value.hide();
-//       }
-//     })
-//     .catch((error) => {
-//       console.dir(error);
-//       setLoading(false);
-//       // 在確認沒有權限的寫法
-//     });
-// };
-const delProduct = (id) => {
+
+const props = defineProps({
+  tempProduct: {
+    type: Object,
+    default() {
+      return {};
+    },
+  },
+  modalType: {
+    type: String,
+    default() {
+      return {};
+    },
+  },
+});
+
+const addEditProduct = (id) => {
+  const method = props.modalType.value === "new" ? "post" : "put";
+  const url =
+    props.modalType.value === "new"
+      ? `${baseUrl}/api/${apiPath}/admin/product`
+      : `${baseUrl}/api/${apiPath}/admin/product/${id}`;
+  const data = { data: props.product };
   setLoading(true);
   axios({
-    method: "DELETE",
-    url: `${baseUrl}/api/${apiPath}/admin/product/${id}`,
+    method,
+    url,
+    data,
   })
     .then(({ data }) => {
       if (data.success) {
@@ -112,104 +46,23 @@ const delProduct = (id) => {
         swal(data.message);
         resetProduct();
         getProducts();
-        delProductModal.value.hide();
+        productModal.value.hide();
       }
     })
     .catch((error) => {
       console.dir(error);
       setLoading(false);
+      // 在確認沒有權限的寫法
     });
 };
-const resetProduct = () => {
-  tempProduct.value = { ...productObj };
-  imagesUrl.value = "";
-};
-const addImagesUrl = () => {
-  if (tempProduct.value.imagesUrl) {
-    tempProduct.value.imagesUrl.push(imagesUrl.value);
-  } else {
-    tempProduct.value.imagesUrl = [imagesUrl.value];
-  }
-  imagesUrl.value = "";
-};
-const delImagesUrl = (index) => {
-  tempProduct.value.imagesUrl.splice(index, 1);
-};
-onMounted(() => {
-  // 從 cookie 取出
-  const token = document.cookie
-    .split("; ")
-    .find((row) => row.startsWith("ttShopToken="))
-    ?.split("=")[1];
-  // 預先載入
-  productModal.value = new Modal(productModalDom.value);
-  delProductModal.value = new Modal(delProductModalDom.value);
-  axios.defaults.headers.common["Authorization"] = token;
-  getProducts();
-});
-</script>
 
+
+
+
+
+
+</script>
 <template>
-  <div class="container">
-    <div class="text-end mt-4">
-      <button @click="openModalHandler('new')" class="btn btn-primary">
-        建立新的產品
-      </button>
-    </div>
-    <table class="table mt-4">
-      <thead>
-        <tr>
-          <th width="120">分類</th>
-          <th>產品名稱</th>
-          <th width="120">原價</th>
-          <th width="120">售價</th>
-          <th width="100">是否啟用</th>
-          <th width="120">編輯</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="product in products" :key="product.id">
-          <td>{{ product.category }}</td>
-          <td>{{ product.title }}</td>
-          <td class="text-end">{{ product.origin_price }}</td>
-          <td class="text-end">{{ product.price }}</td>
-          <td>
-            <span v-if="product.is_enabled" class="text-success">啟用</span>
-            <span v-else>未啟用</span>
-          </td>
-          <td>
-            <div class="btn-group">
-              <button
-                @click="openModalHandler('edit', product.id)"
-                type="button"
-                class="btn btn-outline-primary btn-sm"
-              >
-                編輯
-              </button>
-              <button
-                @click="openModalHandler('delete', product.id)"
-                type="button"
-                class="btn btn-outline-danger btn-sm"
-              >
-                刪除
-              </button>
-            </div>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-    <div v-if="pagination?.total_pages">
-      <Pagination
-        :current_page="pagination.current_page"
-        :total_pages="pagination.total_pages"
-        :has_next="pagination.has_next"
-        :has_pre="pagination.has_pre"
-        @get-page="getProducts"
-      ></Pagination>
-    </div>
-  </div>
-  <!-- Modal -->
-  <!-- 新增 修改 -->
   <div
     id="productModal"
     ref="productModalDom"
@@ -284,10 +137,10 @@ onMounted(() => {
                 </button>
               </div>
               <!-- <div>
-                <button class="btn btn-outline-danger btn-sm d-block w-100">
-                  刪除圖片
-                </button>
-              </div> -->
+                  <button class="btn btn-outline-danger btn-sm d-block w-100">
+                    刪除圖片
+                  </button>
+                </div> -->
             </div>
             <div class="col-sm-8">
               <div class="mb-3">
@@ -410,56 +263,8 @@ onMounted(() => {
       </div>
     </div>
   </div>
-  <!--  刪除 -->
-  <div
-    id="delProductModal"
-    ref="delProductModalDom"
-    class="modal fade"
-    tabindex="-1"
-    aria-labelledby="delProductModalLabel"
-    aria-hidden="true"
-  >
-    <div class="modal-dialog">
-      <div class="modal-content border-0">
-        <div class="modal-header bg-danger text-white">
-          <h5 id="delProductModalLabel" class="modal-title">
-            <span>刪除產品</span>
-          </h5>
-          <button
-            type="button"
-            class="btn-close"
-            data-bs-dismiss="modal"
-            aria-label="Close"
-          ></button>
-        </div>
-        <div class="modal-body">
-          是否刪除
-          <strong class="text-danger"></strong> 商品(刪除後將無法恢復)。
-        </div>
-        <div class="modal-footer">
-          <button
-            type="button"
-            class="btn btn-outline-secondary"
-            data-bs-dismiss="modal"
-            @click="resetProduct"
-          >
-            取消
-          </button>
-          <button
-            @click="delProduct(tempProduct.id)"
-            type="button"
-            class="btn btn-danger"
-          >
-            確認刪除
-          </button>
-        </div>
-      </div>
-    </div>
-  </div>
-  <!-- Modal -->
 </template>
-
-<style scoped lang="scss">
+<style lang="scss" scoped>
 img {
   object-fit: contain;
   max-width: 100%;
