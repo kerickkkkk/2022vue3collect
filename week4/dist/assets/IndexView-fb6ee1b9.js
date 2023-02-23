8c259452c036d832487aaca52d2c47ae78132efc
eric <howcrazys@gmail.com>
2023-02-23 22:44:40 +0800

    page

---
+++ week4/dist/assets/IndexView-fb6ee1b9.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./_plugin-vue_export-helper-c27b6911.js";import{o as n,c}from"./index-8d76ab32.js";const o={};function r(t,a){return n(),c("main",null,"Index")}const f=e(o,[["render",r]]);export{f as default};
