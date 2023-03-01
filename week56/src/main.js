861f68b92b735eb0b1a65f7c109969e6696ba2bd
eric <howcrazys@gmail.com>
2023-03-01 22:32:00 +0800

    feat: 新增 veevalidate rules

--- week56/src/main.js
+++ week56/src/main.js
@@ -6,14 +6,26 @@ import router from "./router";
 // import "./assets/main.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
+import "./scss/utils.scss";
 import VueSweetAlert from "vue-sweetalert2";
 import "sweetalert2/dist/sweetalert2.min.css";
-import { Form, Field, ErrorMessage, defineRule } from "vee-validate";
+import { Form, Field, ErrorMessage, defineRule, configure } from "vee-validate";
 import VeeValidateRules from "@vee-validate/rules";
+import { setLocale, localize } from "@vee-validate/i18n";
+import zhTw from "@vee-validate/i18n/dist/locale/zh_TW.json";
 
 Object.keys(VeeValidateRules).forEach((rule) => {
   defineRule(rule, VeeValidateRules[rule]);
 });
+
+configure({
+  generateMessage: localize({
+    zh_TW: zhTw,
+  }),
+  validateOnInput: true, // 調整為輸入字元立即進行驗證
+});
+
+setLocale("zh_TW");
 
 const pinia = createPinia();
 const app = createApp(App);
