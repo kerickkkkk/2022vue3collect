45018dc6496daf0008d042821b5cb07ddb8a8029
eric <howcrazys@gmail.com>
2023-03-02 21:17:53 +0800

    feat: pinia use router

--- week56/src/main.js
+++ week56/src/main.js
@@ -1,4 +1,4 @@
-import { createApp } from "vue";
+import { createApp, markRaw } from "vue";
 import App from "./App.vue";
 import { createPinia } from "pinia";
 import router from "./router";
@@ -30,10 +30,14 @@ setLocale("zh_TW");
 const pinia = createPinia();
 const app = createApp(App);
 
+// pinia 可以使用 router as plugin
+pinia.use(({ store }) => {
+  store.$router = markRaw(router);
+});
+
 app.use(pinia);
 app.use(router);
 app.use(VueSweetAlert);
-
 app.component("VForm", Form);
 app.component("VField", Field);
 app.component("ErrorMessage", ErrorMessage);
